abc2de34a53219f6ded92243eeef53d4d052c7b2
liuzihao <853378172@qq.com>
2017-11-16 15:47:07 +0800

    流程界面逻辑

--- personal/src/main/java/com/easymi/personal/activity/LoginActivity.java
+++ personal/src/main/java/com/easymi/personal/activity/LoginActivity.java
@@ -2,16 +2,37 @@ package com.easymi.personal.activity;
 
 import android.content.Intent;
 import android.os.Bundle;
+import android.text.Editable;
+import android.text.InputType;
+import android.text.TextWatcher;
+import android.util.Log;
 import android.view.View;
 import android.view.Window;
 import android.view.WindowManager;
 import android.widget.Button;
+import android.widget.CheckBox;
+import android.widget.EditText;
+import android.widget.ImageView;
 import android.widget.TextView;
 
 import com.alibaba.android.arouter.facade.annotation.Route;
 import com.alibaba.android.arouter.launcher.ARouter;
+import com.easymi.component.Config;
 import com.easymi.component.base.RxBaseActivity;
+import com.easymi.component.network.ApiManager;
+import com.easymi.component.network.HttpResultFunc;
+import com.easymi.component.network.MySubscriber;
+import com.easymi.component.network.NoErrSubscriberListener;
+import com.easymi.component.utils.StringUtils;
 import com.easymi.personal.R;
+import com.easymi.personal.entity.Employ;
+import com.easymi.personal.network.McService;
+import com.easymi.personal.result.LoginResult;
+
+import rx.Observable;
+import rx.android.schedulers.AndroidSchedulers;
+import rx.functions.Func1;
+import rx.schedulers.Schedulers;
 
 /**
  * Created by developerLzh on 2017/11/3 0003.
@@ -25,6 +46,16 @@ public class LoginActivity extends RxBaseActivity {
 
     TextView resetPsw;
 
+    EditText editAccount;
+    EditText editPsw;
+
+    ImageView eye;
+
+    CheckBox checkboxAgreement;
+    CheckBox checkboxRemember;
+
+    TextView textAgreement;
+
     @Override
     public int getLayoutId() {
         requestWindowFeature(Window.FEATURE_NO_TITLE);
@@ -36,15 +67,140 @@ public class LoginActivity extends RxBaseActivity {
     public void initViews(Bundle savedInstanceState) {
         loginBtn = findViewById(R.id.login_button);
         loginBtn.setOnClickListener(v -> {
-            ARouter.getInstance().build("/common/WorkActivity")
-                    .navigation();
-            finish();
+            login("13518148468", "1234");
+
         });
+        editAccount = findViewById(R.id.login_et_account);
+        editPsw = findViewById(R.id.login_et_password);
+
+        loginBtn.setEnabled(false);
 
         registerText = findViewById(R.id.login_register);
-        registerText.setOnClickListener(v -> startActivity(new Intent(LoginActivity.this,RegisterActivity.class)));
+        registerText.setOnClickListener(v -> startActivity(new Intent(LoginActivity.this, RegisterActivity.class)));
 
         resetPsw = findViewById(R.id.login_forget);
-        resetPsw.setOnClickListener(v -> startActivity(new Intent(LoginActivity.this,ResetPswActivity.class)));
+        resetPsw.setOnClickListener(v -> startActivity(new Intent(LoginActivity.this, ResetPswActivity.class)));
+
+        eye = findViewById(R.id.eye);
+
+        checkboxAgreement = findViewById(R.id.checkbox_agreement);
+        checkboxRemember = findViewById(R.id.checkbox_remember);
+        textAgreement = findViewById(R.id.text_agreement);
+
+        initEdit();
+
+        initEye();
+
+        initBox();
+    }
+
+    private void initBox() {
+        textAgreement.setOnClickListener(view -> ARouter.getInstance()
+                .build("/component/WebActivity")
+                .withString("url", "www.baidu.com")
+                .withString("title", getString(R.string.login_agreement))
+                .navigation());
+    }
+
+    private boolean eyeOn = false;
+
+    private void initEye() {
+        eye.setOnClickListener(view -> {
+            if (eyeOn) {
+                eye.setImageResource(R.mipmap.ic_close_eye);
+                eyeOn = false;
+                editPsw.setInputType(InputType.TYPE_CLASS_TEXT | InputType.TYPE_TEXT_VARIATION_PASSWORD);
+            } else {
+                eye.setImageResource(R.mipmap.ic_open_eye);
+                eyeOn = true;
+                editPsw.setInputType(InputType.TYPE_TEXT_VARIATION_VISIBLE_PASSWORD);
+            }
+            String input = editPsw.getText().toString();
+            if (StringUtils.isNotBlank(input)) {
+                editPsw.setSelection(input.length());
+            }
+
+        });
+    }
+
+    private void initEdit() {
+
+        editAccount.addTextChangedListener(new TextWatcher() {
+            @Override
+            public void beforeTextChanged(CharSequence charSequence, int i, int i1, int i2) {
+
+            }
+
+            @Override
+            public void onTextChanged(CharSequence charSequence, int i, int i1, int i2) {
+
+            }
+
+            @Override
+            public void afterTextChanged(Editable editable) {
+                if (null != editable && StringUtils.isNotBlank(editable.toString())) {
+                    if (StringUtils.isNotBlank(editPsw.getText().toString())) {
+                        setLoginBtnEnable(true);
+                    } else {
+                        setLoginBtnEnable(false);
+                    }
+                } else {
+                    setLoginBtnEnable(false);
+                }
+            }
+        });
+
+        editPsw.addTextChangedListener(new TextWatcher() {
+            @Override
+            public void beforeTextChanged(CharSequence charSequence, int i, int i1, int i2) {
+
+            }
+
+            @Override
+            public void onTextChanged(CharSequence charSequence, int i, int i1, int i2) {
+
+            }
+
+            @Override
+            public void afterTextChanged(Editable editable) {
+                if (null != editable && StringUtils.isNotBlank(editable.toString())) {
+                    if (StringUtils.isNotBlank(editAccount.getText().toString())) {
+                        setLoginBtnEnable(true);
+                    } else {
+                        setLoginBtnEnable(false);
+                    }
+                } else {
+                    setLoginBtnEnable(false);
+                }
+            }
+        });
+
+    }
+
+    private void setLoginBtnEnable(boolean enable) {
+        loginBtn.setEnabled(enable);
+        if (enable) {
+            loginBtn.setBackgroundDrawable(getResources().getDrawable(R.drawable.corners_button_bg));
+        } else {
+            loginBtn.setBackgroundDrawable(getResources().getDrawable(R.drawable.corners_button_press_bg));
+        }
+    }
+
+    private void login(String name, String psw) {
+        McService api = ApiManager.getInstance().createApi(Config.HOST, McService.class);
+
+        Observable<LoginResult> observable = api
+                .login(name, psw, Config.APP_KEY)
+                .filter(new HttpResultFunc<>(this))
+                .subscribeOn(Schedulers.io())
+                .observeOn(AndroidSchedulers.mainThread());
+
+        mRxManager.add(observable.subscribe(new MySubscriber<>(this, true, true, loginResult -> {
+            Employ employ = loginResult.getEmployInfo();
+            Log.e("okhttp", employ.toString());
+            ARouter.getInstance()
+                    .build("/common/WorkActivity")
+                    .navigation();
+        })));
     }
 }
