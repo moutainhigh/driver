5900d6307776c26e9e92dee0d8f6a298ef1ec241
liuzihao <853378172@qq.com>
2017-12-22 17:59:27 +0800

    map

--- component/src/main/java/com/easymi/component/utils/MapUtil.java
+++ component/src/main/java/com/easymi/component/utils/MapUtil.java
@@ -25,52 +25,52 @@ import java.util.List;
 public class MapUtil {
     public static LatLngBounds getBounds(List<LatLng> latLngs, LatLng center) {
 
-//        if (null != latLngs && latLngs.size() > 0) {
-//
-//            LatLng param = latLngs.get(0);
-//
-//            double maxLat = param.latitude;
-//            double minLat = param.latitude;
-//            double maxLng = param.longitude;
-//            double minLng = param.longitude;
-//
-//
-//            for (LatLng driver : latLngs) {
-//                if (driver.latitude > maxLat) {
-//                    maxLat = driver.latitude;
-//                }
-//                if (driver.latitude < minLat) {
-//                    minLat = driver.latitude;
-//                }
-//
-//                if (driver.longitude > maxLng) {
-//                    maxLng = driver.longitude;
-//                }
-//                if (driver.longitude < minLng) {
-//                    minLng = driver.longitude;
-//                }
-//            }
-//
-//            double maxDiffLat = Math.max(Math.abs(maxLat - center.latitude), (minLat - center.latitude));
-//            double maxDiffLng = Math.max(Math.abs(maxLng - center.longitude), (minLng - center.longitude));
-//
-//            //左上
-//            LatLng leftTopLatLng = new LatLng(center.latitude + maxDiffLat, center.longitude - maxDiffLng);
-//            //右下
-//            LatLng rightBottomLatLng = new LatLng(center.latitude - maxDiffLat, center.longitude + maxDiffLng);
-//
-//            return new LatLngBounds.Builder().include(leftTopLatLng).include(rightBottomLatLng).build();
-//
-//        }
-//        return null;
+        if (null != latLngs && latLngs.size() > 0) {
 
-        LatLngBounds.Builder builder = new LatLngBounds.Builder();
-        for (LatLng latLng : latLngs) {
-            builder.include(latLng);
+            LatLng param = latLngs.get(0);
+
+            double maxLat = param.latitude;
+            double minLat = param.latitude;
+            double maxLng = param.longitude;
+            double minLng = param.longitude;
+
+
+            for (LatLng driver : latLngs) {
+                if (driver.latitude > maxLat) {
+                    maxLat = driver.latitude;
+                }
+                if (driver.latitude < minLat) {
+                    minLat = driver.latitude;
+                }
+
+                if (driver.longitude > maxLng) {
+                    maxLng = driver.longitude;
+                }
+                if (driver.longitude < minLng) {
+                    minLng = driver.longitude;
+                }
+            }
+
+            double maxDiffLat = Math.max(Math.abs(maxLat - center.latitude), (minLat - center.latitude));
+            double maxDiffLng = Math.max(Math.abs(maxLng - center.longitude), (minLng - center.longitude));
+
+            //左上
+            LatLng leftTopLatLng = new LatLng(center.latitude + maxDiffLat, center.longitude - maxDiffLng);
+            //右下
+            LatLng rightBottomLatLng = new LatLng(center.latitude - maxDiffLat, center.longitude + maxDiffLng);
+
+            return new LatLngBounds.Builder().include(leftTopLatLng).include(rightBottomLatLng).build();
+
         }
-        builder.include(center);
+        return null;
 
-        return builder.build();
+//        LatLngBounds.Builder builder = new LatLngBounds.Builder();
+//        for (LatLng latLng : latLngs) {
+//            builder.include(latLng);
+//        }
+//        builder.include(center);
+//
+//        return builder.build();
     }
 
     public static LatLngBounds getBounds(List<LatLng> latLngs) {
