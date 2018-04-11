55621058443833cb819b15696420e6ce9a7a418a
liuzihao <853378172@qq.com>
2018-04-11 18:39:42 +0800

    帮助中心忽略companyId

--- personal/src/main/java/com/easymi/personal/activity/HelpCenterSubActivity.java
+++ personal/src/main/java/com/easymi/personal/activity/HelpCenterSubActivity.java
@@ -66,7 +66,7 @@ public class HelpCenterSubActivity extends RxBaseActivity {
 
         Observable<HelpMenuResult> observable = api
                 .getHelpeSubMenu(Config.APP_KEY,
-                        EmUtil.getEmployInfo().company_id,
+                        (long) 0,
                         1,
                         100,
                         (long) 2)
