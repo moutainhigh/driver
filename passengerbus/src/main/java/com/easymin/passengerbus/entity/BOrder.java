7f4d4b401cb51aab613de61a56094de6121949ee
liuting2 <2548723022@qq.com>
2018-12-27 09:15:30 +0800

    。。。border

---
+++ passengerbus/src/main/java/com/easymin/passengerbus/entity/BOrder.java
@@ -0,0 +1,11 @@
+package com.easymin.passengerbus.entity;
+
+/**
+ * @author:Administrator
+ * @date:2018/12/24 0024下午 1:18
+ * @GitHub:https://github.com/yuxuelian
+ * @email:
+ * @description:
+ */
+public class BcOrder {
+}
