dda95babae713e1bc3a7c4ad97627ce59fa3821b
hufeng <hufeng@andware.cn>
2019-02-14 16:53:40 +0800

    新建定制班车业务模块

---
+++ custombus/src/main/java/com/easymin/custombus/DZBusApiService.java
@@ -0,0 +1,14 @@
+package com.easymin.custombus;
+
+/**
+ * @Copyright (C), 2012-2019, Sichuan Xiaoka Technology Co., Ltd.
+ * @FileName: DZBusApiService
+ * @Author: hufeng
+ * @Date: 2019/2/14 下午1:52
+ * @Description: 定制班车接口地址
+ * @History:
+ */
+public interface DZBusApiService {
+
+
+}
---
+++ custombus/src/main/java/com/easymin/custombus/fragment/CheckTicketFragment.java
@@ -0,0 +1,26 @@
+package com.easymin.custombus.fragment;
+
+import android.os.Bundle;
+
+import com.easymi.component.base.RxBaseFragment;
+import com.easymin.custombus.R;
+
+/**
+ * @Copyright (C), 2012-2019, Sichuan Xiaoka Technology Co., Ltd.
+ * @FileName: CheckTicketFragment
+ * @Author: hufeng
+ * @Date: 2019/2/14 下午2:46
+ * @Description:
+ * @History:
+ */
+public class CheckTicketFragment extends RxBaseFragment{
+    @Override
+    public int getLayoutResId() {
+        return R.layout.fragment_check_ticket;
+    }
+
+    @Override
+    public void finishCreateView(Bundle state) {
+
+    }
+}
