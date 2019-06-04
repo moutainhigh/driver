3ef587eb757ef60b848a5179d84db6909e7234c8
杨洪瑞 <wickedgameray@gmail.com>
2019-06-04 14:21:39 +0800

    compile pandora

--- component/src/main/java/com/easymi/component/network/ApiManager.java
+++ component/src/main/java/com/easymi/component/network/ApiManager.java
@@ -2,7 +2,6 @@ package com.easymi.component.network;
 
 import com.easymi.component.Config;
 import com.easymi.component.app.XApp;
-import com.easymi.component.utils.Log;
 
 import java.io.File;
 import java.util.concurrent.TimeUnit;
@@ -22,6 +21,7 @@ import retrofit2.converter.gson.GsonConverterFactory;
 public class ApiManager {
 
     private OkHttpClient mOkHttpClient;
+
     /**
      * 内部静态类实现单例,且在第一次使用时才加载.
      */
@@ -44,23 +44,17 @@ public class ApiManager {
     private ApiManager() {
         File cacheFile = new File(XApp.getInstance().getCacheDir(), "cache");
         Cache cache = new Cache(cacheFile, 1024 * 1024 * 2); //2M
-        HttpLoggingInterceptor logInterceptor = new HttpLoggingInterceptor();   //拦截器用来输出请求日志方便调试
-        logInterceptor.setLevel(HttpLoggingInterceptor.Level.BODY); //日志输出等级为BODY(打印请求和返回值的头部和body信息)
-
-        Log.e("ApiManger", "ApiManager instance");
 
         Ssl ssl = new Ssl(XApp.getInstance(), "");
-
-        EncryptInterceptor encryptInterceptor = new EncryptInterceptor();
-
         //创建okhttp客户端
         OkHttpClient.Builder builder = new OkHttpClient.Builder();
         builder.readTimeout(16000, TimeUnit.MILLISECONDS)
                 .connectTimeout(16000, TimeUnit.MILLISECONDS)
-                .addInterceptor(encryptInterceptor)  //加密拦截器
+        //拦截器顺序不要改 谁改谁是狗
+                .addInterceptor(new EncryptInterceptor())  //加密拦截器
                 .addInterceptor(new TokenInterceptor())//token拦截器
-                .addInterceptor(logInterceptor) //添加日志拦截器,进行输出日志
-
+                .addInterceptor(new ResponseIntercepter())
+                .addInterceptor(new HttpLoggingInterceptor().setLevel(HttpLoggingInterceptor.Level.HEADERS)) //添加日志拦截器,进行输出日志
                 .retryOnConnectionFailure(true) //失败重连
                 .cache(cache);
 
@@ -83,8 +77,7 @@ public class ApiManager {
     public <T> T createApi(String hostUrl, Class<T> service) {
         Retrofit retrofit = new Retrofit.Builder()
                 .client(mOkHttpClient)
-//                .addConverterFactory(GsonConverterFactory.create()) //添加一个Gson转化
-                .addConverterFactory(KeyGsonConverterFactory.create())
+                .addConverterFactory(GsonConverterFactory.create()) //添加一个Gson转化
                 .addCallAdapterFactory(RxJavaCallAdapterFactory.create()) //添加一个rxjava转换
                 .baseUrl(hostUrl)
                 .build();
---
+++ component/src/main/java/com/easymi/component/network/ResponseIntercepter.java
@@ -0,0 +1,37 @@
+package com.easymi.component.network;
+
+import com.easymi.component.Config;
+import com.easymi.component.utils.AesUtil;
+import com.easymi.component.utils.CsSharedPreferences;
+import com.easymi.component.utils.EncApi;
+import com.easymi.component.utils.URLDecoderUtil;
+
+import java.io.IOException;
+
+import okhttp3.Interceptor;
+import okhttp3.MediaType;
+import okhttp3.Response;
+import okhttp3.ResponseBody;
+import okhttp3.logging.HttpLoggingInterceptor;
+
+public class ResponseIntercepter implements Interceptor {
+
+    private static final String MIME = "application/json; charset=utf-8";
+
+    @Override
+    public Response intercept(Chain chain) throws IOException {
+        Response response = chain.proceed(chain.request());
+        String content = response.body().string();
+
+        if (content.startsWith("{") && content.endsWith("}")) {
+            HttpLoggingInterceptor.Logger.DEFAULT.log(URLDecoderUtil.stringToJSON(content));
+        } else {
+            content = URLDecoderUtil.decode(EncApi.getInstance().dec(new CsSharedPreferences().getString(Config.AES_PASSWORD, AesUtil.AAAAA), content));
+            HttpLoggingInterceptor.Logger.DEFAULT.log(URLDecoderUtil.stringToJSON(content));
+        }
+        return response
+                .newBuilder()
+                .body(ResponseBody.create(MediaType.parse(MIME), content))
+                .build();
+    }
+}
