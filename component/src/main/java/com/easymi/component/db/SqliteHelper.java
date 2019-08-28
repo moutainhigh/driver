7eedc554360da242a896ffaa638ee565460151f6
杨洪瑞 <wickedgameray@gmail.com>
2019-08-29 00:21:30 +0800

    增加版本号

--- component/src/main/java/com/easymi/component/db/SqliteHelper.java
+++ component/src/main/java/com/easymi/component/db/SqliteHelper.java
@@ -13,7 +13,7 @@ public class SqliteHelper extends SQLiteOpenHelper {
 
     public static final String DB_NAME = "data.db";
 
-    private static final int VERSION = 141;
+    private static final int VERSION = 160;
 
     private StringBuffer sqlBuf;
 
